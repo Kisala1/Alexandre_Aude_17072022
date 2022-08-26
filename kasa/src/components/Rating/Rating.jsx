2ddebe0280d295d7d175043f3387654d7df3ede1
Aude Alexandre <alexandre.aude16@gmail.com>
2022-08-26 14:48:39 +0200

    Rating component

--- kasa/src/components/Rating/Rating.jsx
+++ kasa/src/components/Rating/Rating.jsx
@@ -1,13 +1,27 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
 import styles from './Rating.module.scss';
 
-export function Rating() {
-  return (
-    <div>
-      <div className={styles.ratingContainer}>
-        <FontAwesomeIcon icon={faStar} />
-      </div>
-    </div>
-  );
+const StyleRating = styled(FontAwesomeIcon)`
+  color: #ff6060;
+  ${(props) => props.$greyRating && `color: #E3E3E3`}
+`;
+
+export function Rating({ rating }) {
+  const displayRatings = () => {
+    const ratingNumber = parseInt(rating);
+    let content = [];
+
+    for (let index = 1; index <= 5; index++) {
+      if (ratingNumber >= index) {
+        content.push(<StyleRating key={index} icon={faStar} />);
+      } else if (ratingNumber < index) {
+        content.push(<StyleRating key={index} icon={faStar} $greyRating />);
+      }
+    }
+    return <div className={styles.ratingContainer}>{content}</div>;
+  };
+
+  return <div>{displayRatings()}</div>;
 }
