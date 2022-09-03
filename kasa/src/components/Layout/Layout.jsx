4ab65b26eec810c9eff8dde7dec70520c430b90e
Aude Alexandre <alexandre.aude16@gmail.com>
2022-09-03 21:07:56 +0200

    Suppression props

--- kasa/src/components/Layout/Layout.jsx
+++ kasa/src/components/Layout/Layout.jsx
@@ -1,12 +1,12 @@
 import { Header } from '../Header/Header';
 import { Footer } from '../Footer/Footer';
 
-export function Layout({ hasDetails, hasAccueil,footerFixed,  children }) {
+export function Layout({ hasDetails, hasAccueil, children }) {
   return (
     <>
       <Header hasAccueil={hasAccueil} hasDetails={hasDetails} />
       {children}
-      <Footer footerFixed={footerFixed} />
+      <Footer />
     </>
   );
 }
